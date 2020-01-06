596890d308f4e8742ad8e6b606ef5dd6d0cadecb
subbu <padalasubbu80@gmail.com>
2020-01-06 23:01:15 +0530

    Selenium Practice Examples

--- src/test/java/pr6DifferentWaysOfCssSelector/DifferentWaysOfWritingCssSelector.java
+++ src/test/java/pr6DifferentWaysOfCssSelector/DifferentWaysOfWritingCssSelector.java
@@ -48,55 +48,47 @@ public class DifferentWaysOfWritingCssSelector {
 		driver.findElement(By.cssSelector("input.text.primary-text")).clear();
 		Thread.sleep(1000);
 		
-		//		//Locating by xpath using regular expression
-//		driver.findElement(By.xpath("//*[@color='red']")).sendKeys("venkat");
-//		Thread.sleep(1000);
-//		driver.findElement(By.xpath("//*[@color='red']")).clear();
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using contains
-//		driver.findElement(By.xpath("//input[contains(@class,'xpaths')]")).sendKeys("Ganesh");
-//		Thread.sleep(1000);
-//		driver.findElement(By.xpath("//input[contains(@class,'xpaths')]")).clear();
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using starts-with
-//		driver.findElement(By.xpath("//input[starts-with(@id,'last')]")).sendKeys("Suresh");
-//		Thread.sleep(1000);
-//		driver.findElement(By.xpath("//input[starts-with(@id,'last')]")).clear();
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using end-with
-////		driver.findElement(By.xpath("//input[ends-with(@id,'name')]")).sendKeys("Ramesh");
-////		Thread.sleep(1000);
-////		driver.findElement(By.xpath("//input[ends-with(@id,'name')]")).clear();
-////		Thread.sleep(1000);
-//		
-//		//Locating by xpath using parent child relationship
-//		driver.findElement(By.xpath("//div[@id='divtextbox']/div/div/input")).sendKeys("Naresh");
-//		Thread.sleep(1000);
-//		driver.findElement(By.xpath("//div[@id='divtextbox']/div/div/input")).clear();
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using following sibling
-//		String follwingsibbling1 = driver.findElement(By.xpath("//div[@id='child4']//following-sibling::div[1]")).getText();
-//		System.out.println(follwingsibbling1);
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using preceding sibling
-//		String precedingsibbling1 = driver.findElement(By.xpath("//div[@id='child4']//preceding-sibling::div[1]")).getText();
-//		System.out.println(precedingsibbling1);
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using parent
-//		String parentdes = driver.findElement(By.xpath("//div[@id='child4']//parent::div")).getText();
-//		System.out.println(parentdes);
-//		Thread.sleep(1000);
-//		
-//		//Locating by xpath using ancestor
-//		String ancestordes = driver.findElement(By.xpath("//div[@id='child4']//ancestor::div")).getText();
-//		System.out.println(ancestordes);
-//		Thread.sleep(1000);
+		//Locating by CssSelector by using .className
+		driver.findElement(By.cssSelector(".text")).sendKeys("Pavan");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".text")).clear();
+		Thread.sleep(1000);
+				
+		//Locating by CssSelector by using .multiple classNames
+		driver.findElement(By.cssSelector(".text.primary-text.primary-table-text.js-text")).sendKeys("Virat");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".text.primary-text")).clear();
+		Thread.sleep(1000);
+		
+		//Locating by CssSelector using starting text match using symbol ^
+		driver.findElement(By.cssSelector("input[class^='more']")).sendKeys("Kohli");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("input[class^='more']")).clear();
+		Thread.sleep(1000);
+				
+		//Locating by CssSelector using ending text match using symbol $
+		driver.findElement(By.cssSelector("input[class$='xpaths']")).sendKeys("Dravid");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("input[class$='xpaths']")).clear();
+		Thread.sleep(1000);
+				
+		//Locating by CssSelector using anywhere text match using symbol *
+		driver.findElement(By.cssSelector("input[class*=' text ']")).sendKeys("Gambhir");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("input[class*=' text ']")).clear();
+		Thread.sleep(1000);
+		
+		//Locating by CssSelector using Child Locators
+		driver.findElement(By.cssSelector("form>label>input[id='firstname']")).sendKeys("Vikas");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("form>label>input[id='firstname']")).clear();
+		Thread.sleep(1000);
+				
+		//More than one element use findElements and then get()
+		driver.findElements(By.cssSelector("input[class*='text']")).get(0).sendKeys("Khumble");
+		Thread.sleep(1000);
+		driver.findElements(By.cssSelector("input[class*='text']")).get(0).clear();
+		Thread.sleep(1000);
 		
 		driver.close();
 	}
